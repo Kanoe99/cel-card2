581774b1ebe281e5fb34e5ce486cdbd094d7468d
Семён Максимович Гусев <145036030+Kanoe99@users.noreply.github.com>
2024-10-29 13:52:31 +0500

    fix: canvas capture

--- src/renderer/utils/handlers.ts
+++ src/renderer/utils/handlers.ts
@@ -3,7 +3,6 @@ import { DownloadProps, PickedProps } from './interfaces';
 const handleIsPickedItem = ({ isPickedItem, item }: PickedProps) => {
   return item === isPickedItem ? null : item;
 };
-
 const handleFileChange = (
   event: React.ChangeEvent<HTMLInputElement>,
   setImageSrc: React.Dispatch<React.SetStateAction<string | undefined>>,
@@ -13,22 +12,23 @@ const handleFileChange = (
   if (file) {
     const reader = new FileReader();
     reader.onload = (e) => {
-      const filename_edited =
+      const fileName_edited =
         file.name.slice(0, file.name.indexOf('.')) +
         '_edited' +
         file.name.slice(file.name.indexOf('.'));
-      setImageSrc(e.target?.result as string), setFileName(filename_edited);
+
+      setImageSrc(e.target?.result as string), setFileName(fileName_edited);
     };
     reader.readAsDataURL(file);
   }
   event.target.value = '';
 };
 
-const handleSave = ({ uri, fileName }: DownloadProps) => {
-  if (uri && fileName) {
+const handleSave = ({ fileName, stageRef }: DownloadProps) => {
+  if (stageRef?.current && fileName !== undefined) {
     let link = document.createElement('a');
     link.download = fileName;
-    link.href = uri;
+    link.href = stageRef.current?.toDataURL();
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
